f4c4827deaacc5de030229972064451fceac4c9d
Usman <usmanhadier03@gmail.com>
2026-01-31 01:31:04 +0500

    changes done

--- app/layout.js
+++ app/layout.js
@@ -24,7 +24,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Header title={"MIRZA USAMA"}/>
+        <Header title={"Usman"}/>
         {children}
         <Footer/>
       </body>
