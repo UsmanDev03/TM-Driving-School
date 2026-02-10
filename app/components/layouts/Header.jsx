6fa6a1071b41d57eff3541648ffd661b7772928d
Usman <usmanhadier03@gmail.com>
2026-02-10 17:48:15 +0500

    web changes logo

--- app/components/layouts/Header.jsx
+++ app/components/layouts/Header.jsx
@@ -95,7 +95,7 @@ const Header = () => {
                 src="/images/logo/logo-.png"
                 alt="TM Driving School Logo"
                 fill
-                className="object-contain xl:scale-[1.8] scale-[1.3] ml-6"
+                className="object-contain xl:scale-[1.8] scale-[1.3]"
                 priority
               />
             </div>
