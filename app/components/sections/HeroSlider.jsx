7195f907126aa478d79b2723f731028b283ed3c2
Usman <usmanhadier03@gmail.com>
2026-02-12 02:03:39 +0500

    website metatitle-description

--- app/components/sections/HeroSlider.jsx
+++ app/components/sections/HeroSlider.jsx
@@ -6,14 +6,14 @@ import Link from "next/link";
 
 const slides = [
   {
-    image: "/images/hero-1.jpg",
+    image: "/images/hero-1.webp",
     title: "Welcome to TM Drive",
     description: "I offer a high standard of driving tuition at a price that you can afford!",
     buttonText: "Read more",
     link: "/price"
   },
   {
-    image: "/images/hero-2.jpg",
+    image: "/images/hero-2.webp",
     title: "Professional Instruction",
     description: "Expert manual driving lessons tailored to your needs.",
     buttonText: "Get In Touch",
