b6c48c341cb69772527eba3b3bf1b84a00586977
Alishbaakhtar <alishbaakhtarmay2005@gmail.com>
2026-03-24 17:59:00 +0500

    seo

--- app/(pages)/driving-lessons-fulham/page.jsx
+++ app/(pages)/driving-lessons-fulham/page.jsx
@@ -5,7 +5,57 @@ export const metadata = {
   description:
     "Need driving lessons in Fulham? Learn with a calm local instructor. View lesson prices, read pupil reviews, and book your first manual driving lesson.",
 };
-
 export default function Page() {
-  return <DrivingContent />;
+  const fulhamJsonLd = {
+    "@context": "https://schema.org",
+    "@type": "LocalBusiness",
+    name: "TM Drive School Fulham",
+    description:
+      "Professional manual driving school in Fulham providing expert driving lessons and intensive courses.",
+    image:
+      "https://www.tmdriveschool.co.uk/wp-content/uploads/2021/03/TM-Drive-School-Logo.png",
+    url: "https://www.tmdriveschool.co.uk/driving-lessons-fulham",
+    telephone: "+447949488211",
+    priceRange: "££",
+    address: {
+      "@type": "PostalAddress",
+      addressLocality: "Fulham",
+      addressRegion: "London",
+      addressCountry: "GB",
+    },
+    areaServed: {
+      "@type": "City",
+      name: "Fulham",
+    },
+  };
+
+  const fulhamRatingJsonLd = {
+    "@context": "https://schema.org",
+    "@type": "LocalBusiness",
+    name: "TM Drive School",
+    url: "https://www.tmdriveschool.co.uk/driving-lessons-fulham",
+    aggregateRating: {
+      "@type": "AggregateRating",
+      ratingValue: "4.9",
+      bestRating: "5",
+      worstRating: "1",
+      reviewCount: "85",
+    },
+  };
+
+  return (
+    <>
+      <DrivingContent />
+
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(fulhamJsonLd) }}
+      />
+
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(fulhamRatingJsonLd) }}
+      />
+    </>
+  );
 }
--- app/(pages)/page.js
+++ app/(pages)/page.js
@@ -4,14 +4,68 @@ import HeroSlider from "../components/sections/HeroSlider";
 import IntroSection from "../components/sections/IntroSection";
 
 export default function Home() {
+
+  const serviceJsonLd = {
+    "@context": "https://schema.org",
+    "@type": "Service",
+    "serviceType": "Manual Driving Lessons",
+    "provider": {
+      "@type": "LocalBusiness",
+      "name": "TM Drive School",
+      "url": "https://www.tmdriveschool.co.uk/"
+    },
+    "hasOfferCatalog": {
+      "@type": "OfferCatalog",
+      "name": "Driving Tuition Packages",
+      "itemListElement": [
+        {
+          "@type": "Offer",
+          "itemOffered": { "@type": "Service", "name": "Standard Manual Lessons" }
+        },
+        {
+          "@type": "Offer",
+          "itemOffered": { "@type": "Service", "name": "Intensive Driving Courses" }
+        },
+        {
+          "@type": "Offer",
+          "itemOffered": { "@type": "Service", "name": "Pass Plus Training" }
+        }
+      ]
+    }
+  }
+
+
+  const ratingJsonLd = {
+    "@context": "https://schema.org",
+    "@type": "LocalBusiness",
+    "name": "TM Drive School",
+    "url": "https://www.tmdriveschool.co.uk/",
+    "aggregateRating": {
+      "@type": "AggregateRating",
+      "ratingValue": "4.9",
+      "bestRating": "5",
+      "ratingCount": "84",
+      "reviewCount": "84"
+    }
+  };
+
   return (
     <>
-    {/* <Header/> */}
-    <HeroSlider/>
-    <IntroSection/>
-    {/* <Footer/> */}
+      {/* Page content */}
+      {/* <Header/> */}
+      <HeroSlider />
+      <IntroSection />
+      {/* <Footer/> */}
 
+      {/* JSON-LDs */}
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(serviceJsonLd) }}
+      />
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(ratingJsonLd) }}
+      />
     </>
-    
   );
 }
